4ce504b941375f804f20d99a7084ff6b67b90669
rodneyrocket69 <austinhoos8@gmail.com>
2023-04-16 23:32:13 -0400

    pickups!!!!

---
+++ Assets/Scripts/Pickup_Guitar.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup_Guitar : MonoBehaviour
+{
+    public GameObject PickUpText;
+    public GameObject GuitarOnPlayer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GuitarOnPlayer.SetActive(false);
+        PickUpText.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+
+            PickUpText.SetActive(true);
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                this.gameObject.SetActive(false);
+
+                GuitarOnPlayer.SetActive(true);
+
+                PickUpText.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PickUpText.SetActive(false);
+    }
+   
+}
+
