4e53d92d74747bcbe13070d33b6b920e930091f8
rodneyrocket69 <austinhoos8@gmail.com>
2023-05-05 15:28:13 -0400

    synth pick up

---
+++ Assets/Scripts/PickUpSynth.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpSynth : MonoBehaviour
+{
+    public GameObject PickUpText;
+    public GameObject SynthOnPlayer;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SynthOnPlayer.SetActive(false);
+        PickUpText.SetActive(false);
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            PickUpText.SetActive(true);
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                this.gameObject.SetActive(false);
+
+                SynthOnPlayer.SetActive(true);
+
+                PickUpText.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PickUpText.SetActive(false);
+    }
+}
